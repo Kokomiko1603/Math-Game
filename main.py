7730c0e2dc097110f4d79a020f6c540ab6db0e23
Natka <natjun230@gmail.com>
2025-08-01 12:07:42 +0200

    Changed the library, initial frame

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-import pygame
+from tkinter import *
 
-pygame.init()
-# size of the screen, (width, height)
-resolution = (600, 300)
-screen = pygame.display.set_mode(resolution)
-clock = pygame.time.Clock()
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill((220,250,247))
-
-    # puts the conetent on screen
-    pygame.display.flip()
-    clock.tick(60) # limits FPS to 60
-
-pygame.quit()
+# Main application window
+root = Tk()
+root.title = "Mathfight"
