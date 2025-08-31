739537613e5d2c372e9dd47effa99c90e978336b
Natka <natjun230@gmail.com>
2025-08-31 19:19:26 +0200

    Added Level 1 game logic

--- Level_1.py
+++ Level_1.py
@@ -1,7 +1,44 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import random
 
 class Level1(Frame):
+    def generate_task(self):
+
+        num1 = random.randrange(0, 100)
+        num2 = random.randrange(0, 100)
+
+        if num1 >= num2:
+            self.number_1.config(text = num1)
+            self.number_2.config(text = num2)
+        else:
+            self.number_1.config(text = num2)
+            self.number_2.config(text = num1)
+
+        list = ["+", "-"]
+        self.operator.config(text = random.choice(list))
+    
+    def check_answer(self):
+
+        user_answer = int(self.input_box.get())
+        num1 = int(self.number_1.cget("text"))
+        num2 = int(self.number_2.cget("text"))
+        op = self.operator.cget("text")
+        answer = num1 + num2 if op == "+" else num1 - num2
+
+        if user_answer == answer:
+            self.point_counter += 1
+            self.input_box.delete(0, END)
+            self.generate_task()
+        else:
+            self.point_counter -= 1
+            self.input_box.delete(0, END)
+
+        self.points.config(text = "Points: " + str(self.point_counter))
+
+       
+
+
     def __init__( self, parent, controller ):
         super().__init__(parent, background = "#b3ebf2")
         self.controller = controller
@@ -11,8 +48,27 @@ class Level1(Frame):
         self.title = ImageTk.PhotoImage(self.title)
 
         title_label = Label(self, image=self.title, bg="#b3ebf2", borderwidth=0, highlightthickness=0)
-        title_label.place(relx = 0.5, 
-                   rely = 0.2,
-                   anchor = 'center')
+        title_label.place(relx = 0.5, rely = 0.2, anchor = 'center')
+
+        self.point_counter = 0
+        self.points = Label(self, text = "Points: " + str(self.point_counter), bg="#b3ebf2")
+        self.points.place(relx = 0.4, rely = 0.4, anchor = 'center')
+
+        self.number_1 = Label(self, text = "" , bg="#b3ebf2")
+        self.number_1.place(relx = 0.4, rely = 0.5, anchor = 'center')
+
+        self.operator = Label(self, text = "", bg="#b3ebf2")
+        self.operator.place(relx = 0.5, rely = 0.5, anchor = 'center')
+
+        self.number_2 = Label(self, text = "", bg="#b3ebf2")
+        self.number_2.place(relx = 0.6, rely = 0.5, anchor = 'center')
+
+        self.generate_task()
+
+        self.input_box = Entry(self, bg="#b3ebf2")
+        self.input_box.place(relx = 0.5, rely = 0.6, anchor = 'center')
+
+        ok_button = Button(self, text = "OK", command = self.check_answer)
+        ok_button.place(relx = 0.5, rely = 0.7, anchor = 'center')
         
         
